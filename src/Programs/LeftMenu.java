4a9996bffa353779f251727f7f12fc0043ffbe88
suresh.b <suresh.b@DESKTOP-KF32L3Q.infionic.com>
2019-09-12 00:10:12 +0530

    Updated Links Text

--- src/Programs/LeftMenu.java
+++ src/Programs/LeftMenu.java
@@ -2,17 +2,52 @@ package Programs;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 
 public class LeftMenu {
   
 	static WebDriver driver;
+	static Actions a ;
+	static WebDriverWait wait;
 	
 	@Test
-  public void f() {
+  public void f() throws InterruptedException {
+		
+		a = new Actions(driver);
+		
+		wait = new WebDriverWait(driver,60);
+		
+		  wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//tbody//tr//td//a//div[@class='menuIcon']")));
+		  
+		
+		  a.moveToElement(driver.findElement(By.xpath("//tbody//tr//td//a//div[@class='menuIcon']"))).build().perform();
+		  
+		  Thread.sleep(1000);
+		  
+		  wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='divtop']//ul//li//a//div[contains(text(),'HR')]"))).click();
+		
+		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		  
+		  
+		List<WebElement> submodules = driver.findElements(By.cssSelector("div#trvLeftMenuItems > ul >li >div > a"));
+		
+		for(WebElement menu : submodules)
+		{
+			String name = menu.getText();
+			
+			System.out.println(name);
+		}
   }
   @BeforeTest
   public void beforeTest() throws InterruptedException {
@@ -31,7 +66,9 @@ public class LeftMenu {
   
 
   @AfterTest
-  public void afterTest() {
+  public void afterTest() throws InterruptedException {
+	  Thread.sleep(5000);
+	  driver.quit();
   }
 
 }
